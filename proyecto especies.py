e5191d982f149c2e7b8feecdd370576f499bc458
cristhofersanmartin-gif <cristhofer.sanmartin@alu.unach.cl>
2025-10-27 15:15:16 -0300

    Add files via upload
    proyecto de especies hecho por Diego bustos y Cristhofer san martin

---
+++ proyecto especies.py
@@ -0,0 +1,15 @@
+import wx
+import math
+import time
+import random
+
+class especies:
+    def __init__(self, x, y, vida, reproducirse, salto, atacar, correr, comer):
+        self.posicion_x = x
+        self.posicion_y = y
+        self.vida = vida
+        self.reproducirse = reproducirse
+        self.salto = salto
+        self.atacar = atacar 
+        self.correr = correr
+        self.comer = False    

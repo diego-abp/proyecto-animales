de1b4e0079b1d3c40e43567fd9181d210cace5ad
Diego <diegobustos904@gmail.com>
2025-10-28 16:41:58 -0300

    testeo 3

--- proyecto especies.py
+++ proyecto especies.py
@@ -26,7 +26,6 @@ class homniboro(especies):
     def __intit__(self, x, y, vida, reproducirse, salto, atacar, correr, comer):
         super().__init__(self, x, y, vida, reproducirse, salto, atacar, correr, comer)
 # prubeba de commit
-# poj
-
-
-# poj
+class planta(especies):
+    def __init__(self, x, y, vida, reproducirse, comer):
+        super().__init__(self, x, y, vida, reproducirse, comer)
